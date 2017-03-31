aac2cb88e16ca20a5d5b3d5263c91302275a2af9
Derek Miranda <derek.miranda.1992@gmail.com>
2017-03-30 22:42:16 -0700

    Webpack and config

---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+const path = require('path');
+const webpack = require('webpack');
+
+const WDS_PORT = 8000;
+
+module.exports = {
+  entry: [
+    './client',
+  ],
+  output: {
+    filename: 'bundle.js',
+    path: path.join(__dirname, './build'),
+    publicPath: '/build/'
+  },
+  watch: true,
+  plugins: [
+    new webpack.HotModuleReplacementPlugin()
+  ],
+  module: {
+    loaders: [
+      {
+        test: /\.(js|jsx)$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+        include: [ path.resolve(__dirname, './client') ],
+        query: {
+          presets: ['es2015', 'react']
+        }
+      },
+      // {
+      //   test: /(\.css|\.scss)$/,
+      //   include: [ path.resolve(__dirname, './client/css') ],
+      //   loaders: ['style-loader', 'css-loader'],
+      // },
+    ]
+  },
+  devServer: {
+    contentBase: './',
+    inline: true,
+    hot: true,
+  },
+}
