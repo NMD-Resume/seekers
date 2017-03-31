0ff5a3d9fc322e530ba9a18d7ce49684f659e3e3
eveafeline <eveafeline@Naomis-MacBook-Pro.local>
2017-03-31 12:22:22 -0700

    Complete create, find, update, delete user

---
+++ server/UserController.js
@@ -0,0 +1,58 @@
+const User = require('./UserModel');
+const mongoose = require('mongoose');
+
+const UserController = {
+  createUser(req, res) {
+    User.create(req.body, (err, user) => {
+      if (err) {
+        res.status(418).json(err);
+      } else {
+        res.status(200).json(user);
+      }
+      return;
+    })
+  },
+
+  // Get a student from the database and send it in the response
+  // Their first name will be in the request parameter 'name'
+  // This should send the found student
+  getUser(req, res) {
+    User.findOne({username: req.params.username}, (err, user) => {
+      if (err) throw err;
+      if (user === null) {
+        res.status(418).send(err);
+      } else {
+        res.status(200).send(user);
+      }
+    })
+  },
+
+  // Get a student from the database and update the student
+  // The student's first name will be in the request parameter 'name'
+  // The student's new first name will be in the request body
+  updateUser(req, res) {
+    User.findOneAndUpdate({username: req.params.username}, req.body, (err, user) => {
+      if (err) throw err;
+      if (user === null) {
+        res.status(418).send(err);
+      } else {
+        res.status(200).send(user);
+      }
+    });
+  },
+
+  // Delete a student from the database
+  // The student's first name will be sent in the request parameter 'name'
+  // This should send a success status code
+  deleteUser(req, res) {
+    User.deleteOne({ username: req.params.username }, (err, user) => {
+      if (err) {
+        res.status(418).send(err);
+      } else {
+        res.status(200).send();
+      }
+    })
+  },
+};
+
+module.exports = UserController;
