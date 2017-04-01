ca01f5b10215372e385fc1d65379c42eee80eef2
mkhouv <mkhouv@gmail.com>
2017-04-01 02:07:02 -0700

    fixed login issues

--- server/login.js
+++ server/login.js
@@ -6,8 +6,13 @@ login.on('click', (e) => {
   let password = $('#password').val();
   if (username && password) {
     $.post('/login', { username: username, password: password }).then((response) => {
-      console.log(response);
-      if (response === 'Error') $('.message').html('Incorrect username/password').attr('style', 'color: red');
-      else window.location = '/profile';
+      // window.location = '/profile';
+      // console.log(response)
+      if (response === 'Error') {
+        $('.message').html('Incorrect username/password').attr('style', 'color: red');
+      } else {
+        console.log('Hello?');
+        window.location = '/profile';
+      }
   }); 
 }});
