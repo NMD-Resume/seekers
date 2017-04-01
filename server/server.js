e4b846aa430c969ac0a88e3d2aef884778a31846
Derek Miranda <derek.miranda.1992@gmail.com>
2017-04-01 03:36:50 -0700

    Started on resume update, failing with patch request from resume form

--- server/server.js
+++ server/server.js
@@ -26,6 +26,7 @@ app.use(cookieParser());
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH');
     next();
 });
 
