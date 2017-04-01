b4f0aa504adde0c332a2eef78dc6d6e79cbfda1f
eveafeline <eveafeline@Naomis-MacBook-Pro.local>
2017-03-31 17:01:43 -0700

    added routing for authentication

--- server/server.js
+++ server/server.js
@@ -2,8 +2,18 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+const authController = require('./auth/user/authController');
+const cookieController = require('./auth/util/cookieController');
+const sessionController = require('./auth/session/sessionController');
 
-// const userController = require('./UserController');
+
+const userController = require('./UserController');
 
 const PORT = 3000;
 
@@ -18,9 +28,35 @@ app.use(bodyParser.json());
 
 app.use(express.static(__dirname + '/..'));
 
-const userRouter = express.Router();
+app.get('/', cookieController.setCookie, (req, res) => {
+  res.redirect('/resume');
+})
 
-app.post('/', userController.createUser);
+/**
+* signup
+*/
+app.get('/signup', (req, res) => {
+  res.redirect('/signup');
+})
+
+app.post('/signup', userController.createUser);
+
+
+/**
+* login
+*/
+app.post('/login', cookieController.setSSIDCookie, userController.verifyUser);
+
+
+/**
+* Authorized routes
+*/
+app.get('/resume', sessionController.isLoggedIn, (req, res) => {
+  userController.getAllUsers((err, users) => {
+    if (err) throw err;
+    res.render('/resume', { users: users });
+  });
+});
 
 app.get('/:username', userController.getUser);
 
