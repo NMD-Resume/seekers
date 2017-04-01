31f05ead618022c6e6445a7d83fed643f4899579
Derek Miranda <derek.miranda.1992@gmail.com>
2017-04-01 00:18:09 -0700

    Commit server before merge

--- server/server.js
+++ server/server.js
@@ -2,7 +2,6 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-const path = require('path');
 const cookieParser = require('cookie-parser');
 const bcrypt = require('bcryptjs');
 const authController = require('./auth/user/authController');
